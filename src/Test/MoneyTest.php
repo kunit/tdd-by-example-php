af49176a9186877fecc2bbd7dcb2409c0edb5eab
TAKAHASHI Kunihiko <kunihiko.takahashi@gmail.com>
2017-11-20 22:59:02 +0900

    第2章を終了

--- src/Test/MoneyTest.php
+++ src/Test/MoneyTest.php
@@ -10,7 +10,10 @@ class MoneyTest extends TestCase
     public function testMultiplication()
     {
         $five = new Dollar(5);
-        $five->times(2);
-        $this->assertEquals(10, $five->amount);
+        $product = $five->times(2);
+        $this->assertEquals(10, $product->amount);
+
+        $product = $five->times(3);
+        $this->assertEquals(15, $product->amount);
     }
 }
